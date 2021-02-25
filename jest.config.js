1e25cee0851fb91b37908b2981f4b8990c265690
leandroed <leandro-ebc@hotmail.com>
2021-02-25 19:14:29 -0300

    refactor: remove duplication

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,6 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**',
-    '!<rootDir>/src/**/*-protocols.ts',
-    '!**/protocols/**',
     '!**/test/**'
   ],
   coverageDirectory: 'coverage',
