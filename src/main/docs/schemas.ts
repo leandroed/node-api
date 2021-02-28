f52412df5aeec66cad1bd0a7bdfa7042f661b7d7
leandroed <leandro-ebc@hotmail.com>
2021-02-28 13:03:22 -0300

    refactor: move docs to separate files

---
+++ src/main/docs/schemas.ts
@@ -0,0 +1,25 @@
+import {
+  accountSchema,
+  loginParamsSchema,
+  errorSchema,
+  surveyAnswerSchema,
+  surveySchema,
+  surveysSchema,
+  signUpParamsSchema,
+  addSurveyParamsSchema,
+  saveSurveyParamsSchema,
+  surveyResultSchema
+} from './schemas/'
+
+export default {
+  account: accountSchema,
+  loginParams: loginParamsSchema,
+  signUpParams: signUpParamsSchema,
+  addSurveyParams: addSurveyParamsSchema,
+  error: errorSchema,
+  survey: surveySchema,
+  surveys: surveysSchema,
+  surveyAnswer: surveyAnswerSchema,
+  saveSurveyParams: saveSurveyParamsSchema,
+  surveyResult: surveyResultSchema
+}
