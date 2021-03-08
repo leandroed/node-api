ed1843d94299e1e6477f34d498de4afaf65c9f2b
leandroed <leandro-ebc@hotmail.com>
2021-03-08 14:26:55 -0300

    refactor: change stub to spy

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,21 +1,22 @@
 import { AccountModel } from '../models/account'
 import { AddAccountParams } from '../usecases/account/add-account'
 import { AuthenticationParams } from '../usecases/account/authentication'
+import faker from 'faker'
 
 export const mockAddAccountParams = (): AddAccountParams => ({
-  name: 'any_name',
-  email: 'any_email@mail.com',
-  password: 'any_password'
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: faker.internet.password()
 })
 
 export const mockAccountModel = (): AccountModel => ({
-  id: 'any_id',
-  name: 'any_name',
-  email: 'any_email@mail.com',
-  password: 'hashed_password'
+  id: faker.random.uuid(),
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: faker.internet.password()
 })
 
-export const mockFakeAuthentication = (): AuthenticationParams => ({
-  email: 'any_email@mail.com',
-  password: 'any_password'
+export const mockAuthenticationParams = (): AuthenticationParams => ({
+  email: faker.internet.email(),
+  password: faker.internet.password()
 })
--- src/domain/usecases/account/authentication.ts
+++ src/domain/usecases/account/authentication.ts
@@ -4,5 +4,5 @@ export type AuthenticationParams = {
 }
 
 export type Authentication = {
-  auth (authentication: AuthenticationParams): Promise<string>
+  auth (authenticationParams: AuthenticationParams): Promise<string>
 }
