f4444b1d845d4330ec6c39d53c072ee6638b4063
leandroed <leandro-ebc@hotmail.com>
2021-03-11 16:48:32 -0300

    refactor: change return of the load answers by survey

--- src/main/factories/controllers/save-survey-result-controller-factory.ts
+++ src/main/factories/controllers/save-survey-result-controller-factory.ts
@@ -1,8 +1,8 @@
-import { makeLogControllerDecorator, makeDbSaveSurveyResult, makeDbLoadSurveyById } from '@/main/factories'
+import { makeLogControllerDecorator, makeDbSaveSurveyResult, makeDbLoadAnswersBySurvey } from '@/main/factories'
 import { Controller } from '@/presentation/protocols'
 import { SaveSurveyResultController } from '@/presentation/controllers'
 
 export const makeSaveSurveyResultController = (): Controller => {
-  const controller = new SaveSurveyResultController(makeDbLoadSurveyById(), makeDbSaveSurveyResult())
+  const controller = new SaveSurveyResultController(makeDbLoadAnswersBySurvey(), makeDbSaveSurveyResult())
   return makeLogControllerDecorator(controller)
 }
